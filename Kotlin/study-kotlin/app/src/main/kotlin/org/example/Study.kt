c8a553bc8f5b413f58b8e4f356374c3ccfa797c6
kimmo <hemakim0215@naver.com>
2026-09-05 12:28:19 +0900

    .

--- Kotlin/study-kotlin/app/src/main/kotlin/org/example/Study.kt
+++ Kotlin/study-kotlin/app/src/main/kotlin/org/example/Study.kt
@@ -3,12 +3,24 @@ package org.example
 
 class Person(
     val name: String,
+    val person_id: Int,
     var sessionId: Int = 0,
     var connected: Boolean = false,
     var lastLoginAt: Long = 0,
     var role: String = "guest",
 ) {
-    fun connect() {
+    fun Connect() {
         connected = true
+    }
+}
+
+class PersonManager(
+    val persons: MutableMap<Int, Person> = mutableMapOf()
+) {
+    fun newPerson(person: Person) {
+        val pId = person.personId
+        if (!persons.containsKey(pId)) {
+            persons[pId] = person
+        }
     }
 }
