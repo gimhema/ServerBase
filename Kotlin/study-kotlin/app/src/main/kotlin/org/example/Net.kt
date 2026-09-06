c3be795ecfec93d8998faa91eef428b485ab6fd3
kimmo <hemakim0215@naver.com>
2026-09-06 22:58:37 +0900

    .

--- Kotlin/study-kotlin/app/src/main/kotlin/org/example/Net.kt
+++ Kotlin/study-kotlin/app/src/main/kotlin/org/example/Net.kt
@@ -2,3 +2,13 @@
 
 package org.example
 
+data class Connection (
+    val ip : String,
+    val port : Int
+)
+
+class Session (
+    val sessionId : String,
+    val personId : Int,
+    var connection : Connection
+)
